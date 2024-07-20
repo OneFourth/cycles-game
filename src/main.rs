47f396c804d3718010da16d0a3cc9853b4ae848b
Jacques Schutte <jqschutte@gmail.com>
2024-07-20 21:06:19 +0200

    Updated game name

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,3 @@
-// Bevy code commonly triggers these lints and they may be important signals
-// about code quality. They are sometimes hard to avoid though, and the CI
-// workflow treats them as errors, so this allows them throughout the project.
-// Feel free to delete this line.
 #![allow(clippy::too_many_arguments, clippy::type_complexity)]
 
 use bevy::asset::AssetMetaCheck;
@@ -22,8 +18,4 @@ fn main() {
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
-    commands.spawn(SpriteBundle {
-        texture: asset_server.load("ducky.png"),
-        ..Default::default()
-    });
 }
